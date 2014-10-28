a02b9baa0bbffed09c30ec4c7958bf22bcf9fd56
Lance Wicks <lw@judocoach.com>
2014-10-28 11:28:36 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 ajax({ url: 'https://gdata.youtube.com/feeds/api/users/judo/live/events?v=2&status=active&inline=true' }, function(data){
-  var active = data.match(/<yt:status>active<\/yt:status>/);
-  if(active){
+  if(data.match(/<yt:status>active/)){
     simply.title('Ippon.tv is live!');
   } else {
     simple.title('No Ippon.tv right now.');
