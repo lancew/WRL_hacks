d13b88375fe1e92e520f0449f820719e9b9acbe5
Lance Wicks <l.wicks@cv-library.co.uk>
2014-12-22 13:25:42 +0000

    Moving Javascript to sepearate file and improving climbera and Fallers

---
+++ wrl.js
@@ -0,0 +1,77 @@
+var limit = $.url().param( 'limit' ) || 3;
+$.getJSON( 'http://restcountries.eu/rest/v1/all', function( data ) {
+    var html = '';
+    $.each( data, function( key, value ) {
+        html += '<li>&nbsp;<a href="http://lancew.github.io/WRL_hacks/?country=' + value.alpha3Code + '&&limit=9999" class="glyphicon glyphicon-unchecked">' + value.name + '</a></li>';
+    });
+    $( '#nations' ).html( html );
+});
+$.getJSON( 'http://data.judobase.org/api/get_json?params[action]=wrl.by_category&params[category_limit]=' + limit, function( data ) {
+    var total_athletes = 0;
+    var top_mover = {
+        change: '',
+        athlete: ''
+    };
+    var bottom_mover = {
+        change: '',
+        athlete: ''
+    };
+    $.each( data.categories, function( index, value ) {
+        $( '#text' ).append( '<h2>' + value.name + '</h2>' );
+        $.each( value.competitors, function( index2, athlete ) {
+            if ( athlete.country_short == $.url().param( 'country' ) || !$.url().param( 'country' ) ) {
+                delta = Math.abs( athlete.place - athlete.place_prev );
+                if ( athlete.place < athlete.place_prev ) {
+                    delta = '&#8593;' + delta + ' position(s)';
+                    if ( bottom_mover.change < delta ) {
+                        bottom_mover.change = delta;
+                        bottom_mover.athlete = athlete;
+                    }
+                } else if ( athlete.place > athlete.place_prev ) {
+                    delta = '&#8595;' + delta + ' position(s)';
+                    if ( top_mover.change < delta ) {
+                        top_mover.change = delta;
+                        top_mover.athlete = athlete;
+                    }
+                } else {
+                    delta = '';
+                }
+                $( '#text' ).append( athlete.place
+                + ') '
+                + athlete.family_name
+                + ', '
+                + athlete.given_name
+                + ' ('
+                + athlete.country_short
+                + ') '
+                + delta
+                + ' ('
+                + athlete.points
+                + ' points) '
+                + '<br />'
+                );
+                total_athletes++;
+            }
+        });
+        $( '#loading' ).html( '' );
+    });
+    $( '#text' ).prepend( '<p>Total Athletes shown:' + total_athletes );
+    if ( top_mover.athlete && bottom_mover.athlete ) {
+        $( '#no1_faller' ).html( '<p>Top Faller: '
+        + top_mover.change
+        + '  positions '
+        + top_mover.athlete.family_name
+        + ' '
+        + top_mover.athlete.given_name
+        + ' '
+        + top_mover.athlete.weight_name );
+        $( '#no1_climber' ).html( '<p>Top Climber: '
+        + bottom_mover.change
+        + '  positions '
+        + bottom_mover.athlete.family_name
+        + ' '
+        + bottom_mover.athlete.given_name
+        + ' '
+        + bottom_mover.athlete.weight_name );
+    }
+});
