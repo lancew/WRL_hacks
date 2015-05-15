7c0a1706b92c21f2f2c19d6d053af0a5292d5093
Lance Wicks <l.wicks@cv-library.co.uk>
2015-05-15 13:50:00 +0100

    Updating with new IJF widget + JS QA
    Updated the JudoBase widget to show latest videos (new IJF widget) and
    did some Javascript cleanup based on JSCS report. Extended the build
    script to include this also.
    
    Next up is making jshint work properly.

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 # Simple build script so to speak.
 #
-# First compress the js
+# fix/tidy the javascript
+jscs wrl.js -x
+#
+# FIXME: Run JSHint over script
+#jshint wrl.js
+# compress the js
 uglifyjs -c -o wrl-min.js wrl.js --source-map wrl-min.js.map --source-map-url
 # TODO: Then can we compress the css itself?
 
--- wrl.js
+++ wrl.js
@@ -1,126 +1,188 @@
-/*global localStorage:false, $:false */
+/*Global localStorage:false, $:false */
 
-var limit = $.url().param('limit') || localStorage.getItem('limit') || 3;
-var country = $.url().param('country') || localStorage.getItem('country') || '';
-if($.url().param('top')){
-    country = '';
-    limit = $.url().param('top');
+var Limit = $.url().param('limit') || localStorage.getItem('limit') || 3;
+var Country = $.url().param('country') || localStorage.getItem('country') || '';
+if ($.url().param('top')) {
+  Country = '';
+  Limit = $.url().param('top');
 }
 
 // Add the new Judobase widget for the country.
 $(function() {
-  var local_country = country === '' ? 'ijf' : country ;
+  var LocalCountry = Country === '' ? 'ijf' : Country ;
   $('#judobase').html(
-   '<iframe height="350px" scrolling="no" width="200px" frameborder="0" src="http://data.judobase.org/widget/latest_results/'
-   + local_country.toLowerCase()
-   + '?limit=3"></iframe>'
-);
+   '<iframe height="350px"'
+   + 'scrolling="no"'
+   + 'width="200px"'
+   + 'frameborder="0"'
+   + 'src="http://data.judobase.org/widget/latest_videos/'
+   + LocalCountry.toLowerCase()
+   + '?age=sen"></iframe>'
+  );
 });
-$.getJSON('http://data.judobase.org/api/get_json?params[action]=country.get_list', function(data) {
-    "use strict";
-    var html = '';
-    $.each(data, function(key, value) {
-        if (!value.ioc.search('OJU|PJC|EJU|AJU|JUA|IJF')) {
-            return true;
+$.getJSON('http://data.judobase.org/api/'
+        + 'get_json?params[action]=country.get_list',
+        function(data) {
+  'use strict';
+  var html = '';
+  $.each(data, function(key, value) {
+    if (!value.ioc.search('OJU|PJC|EJU|AJU|JUA|IJF')) {
+      return true;
+    }
+    html += '<li>'
+    + '<img src="blank.gif"'
+    + 'class="flag flag-'
+    + value.ioc
+    + '" alt="'
+    + Country
+    + ' flag"> <span>'
+    + '<a href="/?country='
+    + value.ioc + '&limit=9999">' + value.name + '</a></li>';
+  });
+  $('#nations').html(html);
+});
+
+$.getJSON('http://data.judobase.org/api/get_json'
+        + '?params[action]=wrl.by_category'
+        + '&params[category_limit]='
+        + Limit,
+        function(data) {
+  'use strict';
+
+  var TotalAthletes = 0;
+  var BottomMover = {
+    Change: '',
+    Athlete: '',
+  };
+  var TopFemale = {
+    points: 0,
+    athlete: '',
+  };
+  var TopMale = {
+    points: 0,
+    athlete: '',
+  };
+  var TopMover = {
+    Change: '',
+    Athlete: '',
+  };
+
+  if ($.url().param('country')) {
+    localStorage.setItem('country', '' + $.url().param('country'));
+  }
+
+  if ($.url().param('limit')) {
+    localStorage.setItem('limit', $.url().param('limit'));
+  }
+
+  $.each(data.categories, function(index, value) {
+    $('#text').append('<h2>' + value.name + '</h2>');
+    $.each(value.competitors, function(index2, athlete) {
+      if (athlete.country_short === Country || Country === '') {
+        var Delta = Math.abs(athlete.place_prev - athlete.place);
+        var DeltaText;
+        if (athlete.place < athlete.place_prev) {
+          // Climbing
+          DeltaText = '&#8599;' + Delta + ' position(s)';
+          if (BottomMover.Change < Delta) {
+            BottomMover.Change = Delta;
+            BottomMover.DeltaText = DeltaText;
+            BottomMover.Athlete = athlete;
+          }
+        } else if (athlete.place > athlete.place_prev) {
+          // Falling
+          DeltaText = '&#8600;' + Delta + ' position(s)';
+          if (TopMover.Change < Delta) {
+            TopMover.Change = Delta;
+            TopMover.DeltaText = DeltaText;
+            TopMover.Athlete = athlete;
+          }
+        } else {
+          Delta = '';
+          DeltaText = '&#8594; No change in position.';
+
         }
-        html += '<li>'
-        +  '<img src="blank.gif" class="flag flag-'+ value.ioc +'" alt="' + country + ' flag"> <span>'
-        + '<a href="/?country=' + value.ioc + '&limit=9999">' + value.name + '</a></li>';
+
+        if (athlete.gender === 'f'
+            && Number(athlete.points) > Number(TopFemale.points)
+        ) {
+          TopFemale = athlete;
+        }
+        if (athlete.gender === 'm'
+            && Number(athlete.points) > Number(TopMale.points)
+        ) {
+          TopMale = athlete;
+        }
+
+        $('#text').append(athlete.place
+                + ') '
+            + athlete.family_name
+            + ', '
+            + athlete.given_name
+            + ' ('
+            + athlete.country_short
+            + ') '
+            + DeltaText
+            + ' ('
+            + athlete.points
+            + ' points) '
+            + '<br />');
+        TotalAthletes++;
+      }
     });
-    $('#nations').html(html);
-});
+    $('#loading').html('');
+  });
 
-$.getJSON('http://data.judobase.org/api/get_json?params[action]=wrl.by_category&params[category_limit]=' + limit, function(data) {
-    "use strict";
-    var total_athletes = 0;
-    var top_mover = {
-        change: '',
-        athlete: ''
-    };
-    var top_male = {
-        points: 0,
-        athlete: '',
-    };
-    var top_female = {
-        points: 0,
-        athlete: '',
-    };
-    var bottom_mover = {
-        change: '',
-        athlete: ''
-    };
+  if (TopMover.Athlete) {
+    $('#no1_faller').html(
+        TopMover.DeltaText
+        + '  positions: <br />'
+        + TopMover.Athlete.family_name
+        + ' '
+        + TopMover.Athlete.given_name
+        + ' '
+        + TopMover.Athlete.weight_name);
+  }
+  if (BottomMover.Athlete) {
+    $('#no1_climber').html(
+        BottomMover.DeltaText
+        + '  positions  <br />'
+        + BottomMover.Athlete.family_name
+        + ' '
+        + BottomMover.Athlete.given_name
+        + ' '
+        + BottomMover.Athlete.weight_name);
+  }
+  if (TopMale.points) {
+    $('#top_male').html(
+        TopMale.family_name
+        + ' '
+        + TopMale.given_name
+        + ' '
+        + TopMale.points + ' points'
+    );
+  }
+  if (TopFemale.points) {
+    $('#top_female').html(
+        TopFemale.family_name
+        + ' '
+        + TopFemale.given_name
+        + ' '
+        + TopFemale.points + ' points'
+    );
+  }
 
-    if($.url().param('country')){
-        localStorage.setItem('country', "" + $.url().param('country'));
-    }
+  $('#total_athletes').html(TotalAthletes);
 
-    if($.url().param('limit')){
-        localStorage.setItem('limit', $.url().param('limit'));
-    }
-
-    $.each(data.categories, function(index, value) {
-        $('#text').append('<h2>' + value.name + '</h2>');
-        $.each(value.competitors, function(index2, athlete) {
-            if (athlete.country_short === country || country === '' ) {
-                var delta = Math.abs(athlete.place_prev - athlete.place);
-                var delta_text;
-                if (athlete.place < athlete.place_prev) {
-                    // Climbing
-                    delta_text = '&#8599;' + delta + ' position(s)';
-                    if (bottom_mover.change < delta) {
-                        bottom_mover.change = delta;
-                        bottom_mover.delta_text = delta_text;
-                        bottom_mover.athlete = athlete;
-                    }
-                } else if (athlete.place > athlete.place_prev) {
-                    // Falling
-                    delta_text = '&#8600;' + delta + ' position(s)';
-                    if (top_mover.change < delta) {
-                        top_mover.change = delta;
-                        top_mover.delta_text = delta_text;
-                        top_mover.athlete = athlete;
-                    }
-                } else {
-                    delta = '';
-                    delta_text = '&#8594; No change in position.';
-
-                }
-
-                if (athlete.gender === 'f' && Number(athlete.points) > Number(top_female.points)) {
-                    top_female = athlete;
-                }
-                if (athlete.gender === 'm' && Number(athlete.points) > Number(top_male.points)) {
-                    top_male = athlete;
-                }
-
-                $('#text').append(athlete.place + ') ' + athlete.family_name + ', ' + athlete.given_name + ' (' + athlete.country_short + ') ' + delta_text + ' (' + athlete.points + ' points) ' + '<br />');
-                total_athletes++;
-            }
-        });
-        $('#loading').html('');
-    });
-
-    if ( top_mover.athlete) {
-        $('#no1_faller').html(
-            top_mover.delta_text + '  positions: <br />' + top_mover.athlete.family_name + ' ' + top_mover.athlete.given_name + ' ' + top_mover.athlete.weight_name);
-    }
-    if ( bottom_mover.athlete) {
-        $('#no1_climber').html(
-            bottom_mover.delta_text + '  positions  <br />' + bottom_mover.athlete.family_name + ' ' + bottom_mover.athlete.given_name + ' ' + bottom_mover.athlete.weight_name);
-    }
-    if (top_male.points) {
-        $('#top_male').html(
-            top_male.family_name + ' ' + top_male.given_name + ' ' + top_male.points + ' points'
-        );
-    }
-    if (top_female.points) {
-        $('#top_female').html(
-            top_female.family_name + ' ' + top_female.given_name + ' ' + top_female.points + ' points'
-        );
-    }
-    $('#total_athletes').html(total_athletes);
-    if (country) {
-        $('#country').html( '<img src="blank.gif" class="flag flag-'+ country +'" alt="' + country + ' flag"> <span>' + country + '</span>' );
-    }
+  if (Country) {
+    $('#country').html('<img src="blank.gif"'
+            + 'class="flag flag-'
+            + Country
+            + '" alt="'
+            + Country
+            + ' flag"> <span>'
+            + Country
+            + '</span>');
+  }
 
 });
