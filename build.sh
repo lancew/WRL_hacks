420a496d1e77b8b053f6bd26b7aa46ff93c27c00
Lance Wicks <lw@judocoach.com>
2020-03-05 20:57:53 +0000

    Updated build.sh for latest uglify-js

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # FIXME: Run JSHint over script
 eslint wrl.js
 # compress the js
-uglifyjs -c -o wrl-min.js wrl.js --source-map wrl-min.js.map --source-map-url
+uglifyjs -c -o wrl-min.js wrl.js --source-map --source-map-url
 # TODO: Then can we compress the css itself?
 
