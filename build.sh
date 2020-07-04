cdb09cb8932426dced15b399f00fd6b6fb4d3dae
Lance Wicks <lw@judocoach.com>
2020-07-04 15:37:17 +0100

    Starting fresh for ELM

--- build.sh
+++
@@ -1,8 +0,0 @@
-# Simple build script so to speak.
-#
-# FIXME: Run JSHint over script
-eslint wrl.js
-# compress the js
-uglifyjs -c -o wrl-min.js wrl.js --source-map --source-map-url
-# TODO: Then can we compress the css itself?
-
--- test.js
+++
@@ -1,7 +0,0 @@
-ajax({ url: 'https://gdata.youtube.com/feeds/api/users/judo/live/events?v=2&status=active&inline=true' }, function(data){
-  if(data.match(/<yt:status>active/)){
-    simply.title('Ippon.tv is live!');
-  } else {
-    simple.title('No Ippon.tv right now.');
-  }
-});
