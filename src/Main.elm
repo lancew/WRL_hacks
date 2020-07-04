d1def3f142d31632fc48de8406caf504a3ead95c
Lance Wicks <lw@judocoach.com>
2020-07-04 16:10:24 +0100

    Layout defined
    Using https://gist.github.com/avh4/b1a9db12973a2a6ff925552e8b853e69
    Nice wireframing tool by Aaron VonderHaar

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,51 @@
 module Main exposing (main)
 
 import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 
 
 main =
-    Element.layout []
-        <|
-        column [][
-            row[][
-                text "Header"
+    Element.layout [ padding 5, height fill, width fill ] <|
+        column [ width fill, height fill ]
+            [ row [ width fill, padding 10 ]
+                [ placeholder [ width fill, alignTop ] "Header"
+                ]
+            , row [ padding 5, alignTop, height fill, width fill ]
+                [ column [ padding 5, height fill, width (fillPortion 1) ]
+                    [ placeholder [ alignTop, width fill, height fill ] "Left Content"
+                    ]
+                , column [ padding 5, height fill, width (fillPortion 4) ]
+                    [ placeholder [ alignTop, width fill, height fill ] "Right Content"
+                    ]
+                ]
+            , row [ alignBottom, width fill, padding 10 ]
+                [ placeholder [ width fill ] "Footer"
+                ]
             ]
-            , row [][
-                text "content"
-            ]
-            , row [][
-                text "footer"
-            ]
-        ]
 
+
+wireframeTheme =
+    { bg = rgb 0.9 0.9 0.9
+    , frame = rgb 0.5 0.5 0.5
+    , text = rgb 0.3 0.3 0.3
+    }
+
+
+placeholder : List (Attribute msg) -> String -> Element msg
+placeholder attr name =
+    text name
+        |> el
+            [ Border.rounded 5
+            , Border.dotted
+            , Border.color wireframeTheme.frame
+            , Border.width 2
+            , height fill
+            , width fill
+            , padding 20
+            , Background.color wireframeTheme.bg
+            , Font.center
+            , Font.color wireframeTheme.text
+            ]
+        |> el attr
