a8eaa29267332a49de918b346708b6272f1787e3
Lance Wicks <lw@judocoach.com>
2020-07-04 15:57:00 +0100

    Elm-ui setup

--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,20 @@
 module Main exposing (main)
 
-import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Element exposing (..)
+
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Element.layout []
+        <|
+        column [][
+            row[][
+                text "Header"
+            ]
+            , row [][
+                text "content"
+            ]
+            , row [][
+                text "footer"
+            ]
+        ]
 
-type Msg = Increment | Decrement
-
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
