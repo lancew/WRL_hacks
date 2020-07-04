9ec82185bf9195a8632c0875ea11f839c2ae6fa6
Lance Wicks <lw@judocoach.com>
2020-07-04 16:20:57 +0100

    Removed the wireframe

--- src/Main.elm
+++ src/Main.elm
@@ -10,42 +10,17 @@ main =
     Element.layout [ padding 5, height fill, width fill ] <|
         column [ width fill, height fill ]
             [ row [ width fill, padding 10 ]
-                [ placeholder [ width fill, alignTop ] "Header"
+                [ el [ width fill, alignTop, centerX, Font.center ] (text "International Judo Federation World Ranking List")
                 ]
             , row [ padding 5, alignTop, height fill, width fill ]
-                [ column [ padding 5, height fill, width (fillPortion 1) ]
-                    [ placeholder [ alignTop, width fill, height fill ] "Left Content"
+                [ column [ padding 5, height fill, width (fillPortion 1), Border.width 1, Border.rounded 5 ]
+                    [ el [ alignTop, width fill, height fill ] (text "Nations")
                     ]
-                , column [ padding 5, height fill, width (fillPortion 4) ]
-                    [ placeholder [ alignTop, width fill, height fill ] "Right Content"
+                , column [ padding 5, height fill,  width (fillPortion 4), Border.width 1, Border.rounded 5 ]
+                    [ el [ alignTop, width fill, height fill ] (text "Right Content")
                     ]
                 ]
             , row [ alignBottom, width fill, padding 10 ]
-                [ placeholder [ width fill ] "Footer"
+                [ el [ width fill, Font.center ] (text "www.judowrl.com")
                 ]
             ]
-
-
-wireframeTheme =
-    { bg = rgb 0.9 0.9 0.9
-    , frame = rgb 0.5 0.5 0.5
-    , text = rgb 0.3 0.3 0.3
-    }
-
-
-placeholder : List (Attribute msg) -> String -> Element msg
-placeholder attr name =
-    text name
-        |> el
-            [ Border.rounded 5
-            , Border.dotted
-            , Border.color wireframeTheme.frame
-            , Border.width 2
-            , height fill
-            , width fill
-            , padding 20
-            , Background.color wireframeTheme.bg
-            , Font.center
-            , Font.color wireframeTheme.text
-            ]
-        |> el attr
