6e6d90022d7254b821233ebbb86dbd27c542225d
Lance Wicks <lw@judocoach.com>
2020-07-04 17:28:29 +0100

    Adding some data tables

--- src/Main.elm
+++ src/Main.elm
@@ -1,23 +1,71 @@
 module Main exposing (main)
 
+import Browser as Browser
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
+import Http
+
+
+model =
+    { nations = [ "Loading Nations..." ]
+    , athletes = [ "Loading Athletes..." ]
+    }
 
 
 main =
+    view
+
+
+subscriptions =
+    Sub.none
+
+
+view =
     Element.layout [ padding 5, height fill, width fill ] <|
         column [ width fill, height fill ]
             [ row [ width fill, padding 10 ]
-                [ el [ width fill, alignTop, centerX, Font.center ] (text "International Judo Federation World Ranking List")
-                ]
-            , row [ padding 5, alignTop, height fill, width fill ]
-                [ column [ padding 5, height fill, width (fillPortion 1), Border.width 1, Border.rounded 5 ]
-                    [ el [ alignTop, width fill, height fill ] (text "Nations")
+                [ el
+                    [ width fill
+                    , alignTop
+                    , centerX
+                    , Font.center
+                    , Font.bold
+                    , Font.size 36
+                    , Font.family
+                        [
+                          Font.serif
+                        ]
                     ]
-                , column [ padding 5, height fill,  width (fillPortion 4), Border.width 1, Border.rounded 5 ]
-                    [ el [ alignTop, width fill, height fill ] (text "Right Content")
+                    (text "International Judo Federation World Ranking List")
+                ]
+            , row [ padding 5, alignTop, height fill, width fill, spacing 5 ]
+                [ column [ padding 5, height fill, width (fillPortion 1), Border.width 1, Border.rounded 5 ]
+                    [ Element.table [ alignTop, height fill ]
+                        { data = List.sort model.nations
+                        , columns =
+                            [ { header = el [ Font.bold ] (Element.text "Nations")
+                              , width = fill
+                              , view =
+                                    \nation ->
+                                        el [] (Element.text nation)
+                              }
+                            ]
+                        }
+                    ]
+                , column [ padding 5, height fill, width (fillPortion 4), Border.width 1, Border.rounded 5 ]
+                    [ Element.table [ alignTop, height fill ]
+                        { data = List.sort model.athletes
+                        , columns =
+                            [ { header = el [ Font.bold ] (Element.text "Athletes")
+                              , width = fill
+                              , view =
+                                    \athlete ->
+                                        el [] (Element.text athlete)
+                              }
+                            ]
+                        }
                     ]
                 ]
             , row [ alignBottom, width fill, padding 10 ]
