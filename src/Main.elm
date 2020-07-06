71a2685d9c5c88e5ca0539c39ac56543183842b1
Lance Wicks <lw@judocoach.com>
2020-07-06 21:18:07 +0100

    Successfully pulling data from API!!
    That was painful!
    I have the code I am using as a model commented out in the source;
    I use a button and it gets the data.
    
    Next lets make it better.

--- src/Main.elm
+++ src/Main.elm
@@ -12,6 +12,7 @@ import Element.Input as Input
 import Html exposing (Html, button, div)
 import Html.Events exposing (onClick)
 import Http
+import Json.Decode
 
 
 
@@ -33,13 +34,13 @@ main =
 
 
 type alias Model =
-    { nations : List String
+    { nations : List Nation
     , athletes : List String
     }
 
 
 init _ =
-    ( { nations = [ "Loading Nations..." ]
+    ( { nations = []
       , athletes = [ "Loading Athletes..." ]
       }
     , Cmd.none
@@ -61,14 +62,25 @@ subscriptions model =
 type Msg
     = GetNations
     | GetAthletes
+    | FetchNations (Result Http.Error (List Nation))
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GetNations ->
-            ( { model | nations = getNationsFromAPI }, Cmd.none )
+            ( model, getNationsFromAPI )
+
         GetAthletes ->
             ( { model | athletes = getAthletesFromAPI }, Cmd.none )
+
+        FetchNations result ->
+            case result of
+                Ok nations ->
+                    ( { model | nations = nations }, Cmd.none )
+
+                Err _ ->
+                    ( { model | nations = [] }, Cmd.none )
 
 
 
@@ -101,13 +113,13 @@ view model =
                         }
                     , Element.table
                         [ alignTop, height fill, padding 5 ]
-                        { data = List.sort model.nations
+                        { data = model.nations
                         , columns =
                             [ { header = el [ Font.bold ] (Element.text "Nations")
                               , width = fill
                               , view =
                                     \nation ->
-                                        el [] (Element.text nation)
+                                        el [] (Element.text nation.name)
                               }
                             ]
                         }
@@ -137,7 +149,145 @@ view model =
 
 
 getNationsFromAPI =
-    [ "Australia", "Zimbabwe", "New Zealand" ]
+    Http.get
+        { url = "https://data.judobase.org/api/get_json?params[action]=country.get_list"
+        , expect = Http.expectJson FetchNations nationsDecoder
+        }
+
+
+nationDecoder =
+    Json.Decode.map3 Nation
+        (Json.Decode.field "id_country" Json.Decode.string)
+        (Json.Decode.field "name" Json.Decode.string)
+        (Json.Decode.field "ioc" Json.Decode.string)
+
+
+nationsDecoder =
+    Json.Decode.map identity
+        (Json.Decode.list nationDecoder)
+
+
+type alias Nation =
+    { id_country : String, name : String, ioc : String }
+
 
 getAthletesFromAPI =
     [ "Wicks, Lance", "Kano, Jigoro", "Koga, Toshihiko", "Adams, Sam" ]
+
+
+
+{--
+
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, text, ul, li, button, span)
+import Html.Events exposing (onClick)
+import Json.Decode
+import Http
+
+
+type alias Post =
+    {id_country: String, name: String, ioc: String }
+
+type alias Model = 
+    { posts: List Post, uiState: UiState, error: Maybe String }
+
+
+initialModel : Model
+initialModel =
+    { posts = [], uiState = Init,  error = Nothing }
+
+init: (Model, Cmd Msg)
+init =
+    (initialModel, Cmd.none)
+
+type UiState
+    = Init
+    | Loading
+    | Success
+    | Failure
+
+
+type Msg
+    = InitFetchPosts
+    | FetchPosts (Result Http.Error (List Post))
+
+
+fetchPosts: Cmd Msg
+fetchPosts =
+        Http.get
+            { url = "https://data.judobase.org/api/get_json?params[action]=country.get_list"
+            , expect = Http.expectJson FetchPosts postCollectionDecoder 
+            }
+
+
+postDecoder : Json.Decode.Decoder Post
+postDecoder =
+    Json.Decode.map3 Post
+        (Json.Decode.field "id_country" Json.Decode.string)
+        (Json.Decode.field "name" Json.Decode.string)
+        (Json.Decode.field "ioc" Json.Decode.string)
+
+
+postCollectionDecoder : Json.Decode.Decoder (List Post)
+postCollectionDecoder =
+    Json.Decode.map identity
+        (Json.Decode.list postDecoder)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+         InitFetchPosts ->
+            ( { model | uiState = Loading }, fetchPosts )
+
+         FetchPosts result ->
+            case result of
+                Ok posts ->
+                    ( { model | uiState = Success, posts = posts }, Cmd.none )
+
+                Err _ ->
+                    ( { model | uiState = Failure, error = Just "Error" }, Cmd.none )
+
+renderItem: Post -> Html Msg
+renderItem post =
+    li [] [text post.name]
+    
+renderData: Model -> Html Msg
+renderData model =
+    case model.uiState of
+        Init ->
+            span [] []
+        Loading ->
+            span [] [text "Loading..."]
+            
+        Success -> 
+          ul [] (List.map (\post -> renderItem post) model.posts)
+          
+        Failure ->
+            case model.error of 
+                Just error -> 
+                    span [] [text error]
+
+                Nothing ->
+                    span [] []
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [onClick InitFetchPosts] [text "Fetch Posts"]
+        , renderData model
+        ]
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
+
+--}
